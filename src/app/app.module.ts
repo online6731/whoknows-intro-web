8c5ad548684c262d579fc1e73d01b167a8d46822
farhadPO <44239878+farhadPO@users.noreply.github.com>
2019-07-20 17:04:22 +0430

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MainComponentComponent } from './main-component/main-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MainComponentComponent
   ],
   imports: [
     BrowserModule,
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Component } from '@angular/core';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
